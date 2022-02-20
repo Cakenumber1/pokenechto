e5f03f9664b8a28167fa902c5a41f77b31726171
qvazi <qvazi-s@ya.ru>
2022-02-21 00:40:08 +0500

    [inventory] update inventoryHelpers

--- helpers/inventoryHelpers.ts
+++ helpers/inventoryHelpers.ts
@@ -1,29 +1,6 @@
-import axios from 'axios';
+import React from 'react';
 
 export const naturalNumberPattern = /^[1-9]{1}[0-9]{0,}$/;
-export const adjacentSlide = (
-  pageQuery: number,
-  slideIndex: number,
-  pagesCount: number,
-) => {
-  const slideIndexLikePage = slideIndex + 1;
-  return (
-    (pageQuery === 1 && slideIndexLikePage <= 2)
-    || (pageQuery > 1
-      && (pageQuery === slideIndexLikePage - 1
-        || pageQuery === slideIndexLikePage
-        || pageQuery === slideIndexLikePage + 1))
-    || (pageQuery === pagesCount && slideIndexLikePage >= pagesCount - 1)
-  );
-};
-// swr
-export const getInventoryPagesLengthKey = '/api/inventory';
-export const fetchInventoryPagesLength = (url: string) => axios
-  .head<number>(url)
-  .then((response) => Number(response.headers['x-inventory-pages-length']));
-
-export const getInventoryByPageKey = (page: number) => `/api/inventory/${page}`;
-export const fetchInventoryByPage = (url: string) => axios.get<PokemonCollectionItemType[]>(url).then((response) => response.data);
 
 // types
 export type PokemonType = {
@@ -40,9 +17,14 @@ export type PokemonType = {
   // expType: enum;
 };
 
-export type PokemonCollectionItemType = PokemonType & { collectionId: number };
+export type CollectionItemType = PokemonType & { collectionId: number };
 
-export type PokemonCollectionItemProp = Pick<
-PokemonCollectionItemType,
-'collectionId' | 'name' | 'id' | 'sprite'
->;
+export type HandleClickCard = (
+  event: React.MouseEvent<HTMLDivElement>,
+  collectionItem: Partial<CollectionItemType>
+) => void;
+
+export type HandleClickPopoverControls = (
+  event: React.MouseEvent<HTMLButtonElement>,
+  buttonClicked: 'info' | 'gift'
+) => void;
