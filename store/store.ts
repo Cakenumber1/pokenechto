da5b535052878b569ccaf168a43ef98fde39fda5
qvazi <qvazi-s@ya.ru>
2022-02-21 00:38:30 +0500

    [inventory] impl inventoryApi service

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,18 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { inventoryApi } from 'store/service';
 
 import counterReducer from './slices/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [inventoryApi.reducerPath]: inventoryApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(inventoryApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
