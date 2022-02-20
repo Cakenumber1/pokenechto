9eb8fa8df55c363d9143d3dd7f1a27c076b08f82
qvazi <qvazi-s@ya.ru>
2022-02-21 00:47:22 +0500

    [inventory] refactor /api/inventory

--- pages/api/inventory/index.ts
+++ pages/api/inventory/index.ts
@@ -4,10 +4,22 @@ import fakeDB from 'pages/api/fakeDB';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
-    case 'HEAD':
-      res.setHeader('x-inventory-pages-length', fakeDB.inventory.getPages());
+    case 'GET': {
+      const results = fakeDB.inventory.getByPage(Number(req.query.page));
+      if (results) {
+        return res.status(200).json({
+          count: fakeDB.inventory.getLength(),
+          results,
+        });
+      }
+      break;
+    }
+    case 'DELETE': {
+      fakeDB.inventory.deleteCollectionItem(req.body.collectionId);
       return res.status(200).end();
+    }
     default:
-      return res.status(400).end();
   }
+
+  return res.status(400).end();
 }
