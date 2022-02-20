7a30858323a15b1dcdefe566de7b83ded28353f5
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-02-21 02:05:43 +0300

    [shop] #3 impl maps for ModalComponent

---
+++ helpers/types/statsMap.ts
@@ -0,0 +1,17 @@
+export const colorMap = new Map([
+  ['hp', 'red'],
+  ['attack', 'orange'],
+  ['defense', 'aqua'],
+  ['special-attack', 'yellow'],
+  ['special-defense', 'lightblue'],
+  ['speed', 'white'],
+]);
+
+export const namesMap = new Map([
+  ['hp', 'HP'],
+  ['attack', 'ATC'],
+  ['defense', 'DEF'],
+  ['special-attack', 'Spec.A'],
+  ['special-defense', 'Spec.D'],
+  ['speed', 'SPD'],
+]);
