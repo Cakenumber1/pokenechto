ae74b787028b4b44a6662c1feb19616caf7bc24d
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-02-21 16:55:13 +0300

    rm pages/inventory.jsx, fixed double fetch in Shop

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -11,7 +11,7 @@ const Shop: NextPage = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchPokemonsThunk());
-  }, [dispatch]);
+  }, []);
   return (<OnloadOverlayComponent />);
 };
 
